0333782d55c94aa4a3cb40aaefbe79d6faf2c902
Victor Diaz <diazvaldiviav@gmail.com>
2024-06-10 10:38:49 -0400

    tengo que arreglar el controlador de los invoices

---
+++ WebApplication1/Controllers/InvoiceController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : BaseApiController
+    {
+        private readonly BookContext _context;
+
+        public InvoiceController (BookContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetInvoice")]
+
+        public async Task<ActionResult> GetInvoice()
+        {
+            var invoice = await _context.Invoices
+                 .Include(i => i.Items)
+                 .FirstOrDefaultAsync(); ;
+
+            if (invoice == null) { return NotFound(); }
+
+            return Ok(invoice);
+        }
+
+        public async Task<ActionResult<Invoice>> AddItemToInvoice(int invoiceId, string name, string description)
+        {
+
+            //get invoice || create item
+            var invoice = await RetrieveInvoice(invoiceId);
+
+            //add item
+            invoice.AddItem(name, description);
+
+            //save changes
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return CreatedAtRoute("GetInvoice", invoice);
+
+            return BadRequest(new ProblemDetails { Title = "Problem saving item to invoice" });
+        }
+
+    
+
+        private async Task<Invoice> RetrieveInvoice(int id)
+        {
+            if (id == null)
+            {
+                return await _context.Invoices
+                 .Include(i => i.Items)
+                 .FirstOrDefaultAsync();
+            }
+
+            return await _context.Invoices.Include(i => i.Items).FirstOrDefaultAsync(item => item.Id == id);
+           
+        }
+    }
+}
--- WebApplication1/Models/Invoice.cs
+++ WebApplication1/Models/Invoice.cs
@@ -7,11 +7,22 @@
         public DateOnly DateIssue { get; set; }
         public string InvoiceNumberId { get; set; }
         public int CompanyId { get; set; }
-        public Item Item { get; set; }
+        // public Item Item { get; set; }
 
+        public List<Item> Items { get; set; } = new ();
         public string BillPicture { get; set; }
 
         public long TotalPrice { get; set; } // PRICE + TAXES
+
+
+        public void AddItem(string name, string description = "NEW ITEM")
+        {
+            if (name != null)
+            {
+                Items.Add(new Item { Name = name, Description = description});
+            }
+
+        }
     
     }
 }
